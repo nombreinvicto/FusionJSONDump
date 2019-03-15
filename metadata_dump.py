cd7ab98779e8d0b092f1ad8e69fcc0f7de9c74c5
nombreinvicto <mahmud.buet.me@gmail.com>
2019-03-15 01:21:43 -0400

    added STL export routine

--- metadata_dump.py
+++ metadata_dump.py
@@ -1,9 +1,8 @@
 import adsk.core as adskc
 import adsk.fusion as adskf
-import adsk.cam as adskam
 import traceback
 import adsk
-import ctypes, os, json
+import os, json
 
 # default api unit is cm/ radian
 # global variables
@@ -14,13 +13,11 @@ class JsonDumpButtPressEvtHandler(adskc.CommandCreatedEventHandler):
     def __init__(self):
         super().__init__()
 
-    def notify(self, args: adskc.CommandCreatedEventArgs):
+    @staticmethod
+    def notify(args: adskc.CommandCreatedEventArgs):
+        app = adskc.Application.get()
+        ui = app.userInterface
         try:
-            # this function sets up documents and initialises design global vars
-            # setting up app and document
-            app = adskc.Application.get()
-            ui = app.userInterface
-            doc = app.activeDocument
             current_product = app.activeProduct
 
             if not isinstance(current_product, adskf.Design):
@@ -40,7 +37,7 @@ class JsonDumpButtPressEvtHandler(adskc.CommandCreatedEventHandler):
                                                     '',
                                                     False)
             booleanInput.isFullWidth = True
-            booleanInput.text = 'Initiate Dump'
+            booleanInput.text = '        Initiate Dump        '
 
             # executes as soon as inputs change
             onInputChanged = JsonDumpInputChangedHandler()
@@ -61,7 +58,8 @@ class JsonDumpInputChangedHandler(adskc.InputChangedEventHandler):
     def __init__(self):
         super().__init__()
 
-    def notify(self, args: adskc.InputChangedEventArgs):
+    @staticmethod
+    def notify(args: adskc.InputChangedEventArgs):
         try:
             app = adskc.Application.get()
             ui = app.userInterface
@@ -74,10 +72,6 @@ class JsonDumpInputChangedHandler(adskc.InputChangedEventHandler):
 
             # find all components
             allComponents = design.allComponents
-
-            # Returns the collection of command inputs that are
-            # associated with the command this event is being fired for
-            inputs = args.inputs
 
             # Returns the command input that has just changed.
             cmdInput = args.input
@@ -95,11 +89,23 @@ class JsonDumpInputChangedHandler(adskc.InputChangedEventHandler):
                     rootComp = design.rootComponent
                     rootCompName = rootComp.name
                     param_dict = min_max_routine(ui, allComponents)
-
+                    if not param_dict:
+                        return
+                    # dump json
                     with open(folderPath + '\\' +
                               rootCompName + '.json', 'w') as outfile:
                         json.dump(param_dict, outfile)
                     ui.messageBox('Json Dump Complete!')
+
+                    # dump stl
+                    exprtMgr = adskf.ExportManager.cast(design.exportManager)
+                    stlOptions = exprtMgr.createSTLExportOptions(rootComp)
+                    stlOptions.meshRefinement = adskf.MeshRefinementSettings \
+                        .MeshRefinementMedium
+                    stlOptions.filename = folderPath + '\\' \
+                                          + rootCompName + '.stl'
+                    exprtMgr.execute(stlOptions)
+                    ui.messageBox('STL Dump Complete!')
 
                 else:
                     pass
@@ -114,7 +120,8 @@ class OKEventHandler(adskc.CommandEventHandler):
     def __init__(self):
         super().__init__()
 
-    def notify(self, args):
+    @staticmethod
+    def notify(args):
         app = adsk.core.Application.get()
         ui = app.userInterface
 
@@ -185,7 +192,7 @@ def min_max_routine(ui, allComponents):
                             'cancelling json dump '
                             'sequence')
                         # adsk.terminate()
-                        return
+                        return {}
 
                 tdict["currentValue"] = tparam.value
                 param_dict[tparam.name] = tdict
@@ -194,7 +201,6 @@ def min_max_routine(ui, allComponents):
 
 def run(context):
     try:
-        # this function sets up documents and initialises design global vars
         # setting up app and document - dont generate error on empty doc
         app = adskc.Application.get()
         ui = app.userInterface
